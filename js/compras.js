12f4cfdec97dfd4a86a42ee63c46e0c07108e72e
Ale2301 <alejo.2001@live.com.ar>
2023-08-11 02:51:16 -0300

    feat: el boton de reiniciar funciona

--- js/compras.js
+++ js/compras.js
@@ -3,7 +3,8 @@ const itemsParaVender = [
     {
         item:"botiquin", //nombre del item
         displayName:"Botiquín inferior", //titulo del item
-        descripcion:"Un pequeño botiquín. Sana 10% de tu salud", //descripcion del item   
+        descripcion:"Un pequeño botiquín. Sana 10% de tu salud", //descripcion del item
+        precioBase:3,  
         precio:3, //precio base del item
         imagen: "/../assets/tienda/Botiquin.png", //URL de la imagen
         escalado:2, //cuánto escala el precio
@@ -12,6 +13,7 @@ const itemsParaVender = [
         item:"balas",
         displayName:"x5 balas globales",
         descripcion:"5 geniales balas. Sirven para cualquier arma",
+        precioBase:5,
         precio:5,
         imagen: "/../assets/tienda/Bala_pistola.png",
         escalado:2,
@@ -20,6 +22,7 @@ const itemsParaVender = [
         item:"metralleta", //nombre del item
         displayName:"Arma : Metralleta",
         descripcion:"Una increíble arma. Viene con sus propias balas. Dispara de a ráfagas de 4  balas por vez. Posee 15 balas. 500ms de recarga. ", //descripcion del item   
+        precioBase:30,
         precio:30, //precio base del item
         imagen: "/../assets/tienda/Metralleta.png", //URL de la imagen
         escalado:false, //cuánto escala el precio
@@ -28,6 +31,7 @@ const itemsParaVender = [
         item:"daño",
         displayName:"Letalidad extra",
         descripcion:"Aumentas el daño de tus balas en 1. Actualmente: " + player.dañoPorTiro,  
+        precioBase:20,
         precio:20,
         imagen: "",
         escalado:2,
@@ -36,6 +40,7 @@ const itemsParaVender = [
         item:"velocidadRecarga",
         displayName:"Recarga acelerada",
         descripcion:"Tus armas se recargan un 10% más rápido. Actualmente: " + player.velocidadRecarga + "%",  
+        precioBase:10,
         precio:10,
         imagen: "",
         escalado:3,
@@ -44,6 +49,7 @@ const itemsParaVender = [
         item:"mejoraBalas",
         displayName:"Cartuchos de ensueño",
         descripcion:"Aumenta la posibilidad de recargar sin consumir balas. Solo funciona en recargas completas. Actualmente: " + player.mejoraBalas + "%",  
+        precioBase:80,
         precio:80,
         imagen: "",
         escalado:5,
--- js/recolectarMonedas.js
+++ js/recolectarMonedas.js
@@ -1,14 +1,19 @@
-function recolectarMonedas(){
+function recolectarMonedas(finalPartida){
     for (let i=0;i<monedasPorRecolectar.length;i++){
         let monedaActual = monedasPorRecolectar[i]
         monedaActual.div.classList.add("catchCoins")
-        monedaActual.div.addEventListener("animationend",function(e){
-            monedaActual.cantidad = Math.floor(monedaActual.cantidad/2)
-            player.monedas = player.monedas + monedaActual.cantidad
-            console.log("sumando la moneda que da " + monedaActual.cantidad)
+        if (finalPartida){
             monedaActual.div.remove()
-            actualizarValoresPantalla()
-        })
+        }
+        else{
+            monedaActual.div.addEventListener("animationend",function(e){
+                monedaActual.cantidad = Math.floor(monedaActual.cantidad/2)
+                player.monedas = player.monedas + monedaActual.cantidad
+                console.log("sumando la moneda que da " + monedaActual.cantidad)
+                monedaActual.div.remove()
+                actualizarValoresPantalla()
+            })
+        }
     }
     monedasPorRecolectar = []
 }
